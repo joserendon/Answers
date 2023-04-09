f5e04ff2638d75ef7a3da41a75ad0006b0da1428
jose.rendon <jose.rendon@praxedes-group.com>
2023-04-08 20:50:51 -0500

    add prop Id_Guid  in PaginationDTo

--- Answers.Shared/DTOs/PaginationDTO.cs
+++ Answers.Shared/DTOs/PaginationDTO.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
 
+        public Guid? Id_Guid { get; set; }
+
         public int Page { get; set; } = 1;
 
         public int RecordsNumber { get; set; } = 10;
