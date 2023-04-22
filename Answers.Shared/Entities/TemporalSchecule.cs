01d40a770e357a9819d7e1563895d831ac84b2e5
jalexandergar <jeissongarcia164711@correo.itm.edu.co>
2023-04-22 08:46:22 -0500

    add temporalSchedule

--- Answers.Shared/Entities/TemporalSchecule.cs
+++ Answers.Shared/Entities/TemporalSchecule.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Answers.Shared.Entities
 {
-    internal class TemporalSchecule
+    public class TemporalSchecule
     {
+        public int Id { get; set; }
+
+        public User? User { get; set; }
+
+        public string? UserId { get; set; }
+
+        public Questionnaire? Questionnaire { get; set; }
+
+        public string QuestionnaireId  { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public float Quantity { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Comentarios")]
+        public string? Remarks { get; set; }
+
+
     }
 }
